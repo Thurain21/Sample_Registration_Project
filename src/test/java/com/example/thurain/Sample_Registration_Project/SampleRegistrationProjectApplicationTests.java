97005abd80e96af2fbef5f69850e83d943616e12
Thurain21 <thurainkosg@gmail.com>
2024-10-27 22:31:22 +0800

    add src files

---
+++ src/test/java/com/example/thurain/Sample_Registration_Project/SampleRegistrationProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thurain.Sample_Registration_Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleRegistrationProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
